74a479c2b6aff234bc50e9c5578eec78b7b425c5
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-04-16 21:56:12 -0400

    important links

--- metal-wallet/src/components/Counter/Counter.js
+++ metal-wallet/src/components/Counter/Counter.js
@@ -3,6 +3,9 @@ import {useCountValue, useActions} from '../../features/counter';
 import classes from './Counter.module.css';
 
 //https://levelup.gitconnected.com/fetch-an-api-with-react-hooks-79d509a052a0
+//https://www.robinwieruch.de/react-hooks-fetch-data
+//https://medium.com/better-programming/how-to-fetch-data-from-an-api-with-react-hooks-9e7202b8afcd
+//https://blog.bitsrc.io/making-api-calls-with-react-hooks-748ebfc7de8c
 
 const IdForm = props => {
   const [id, setid] = useState('');
